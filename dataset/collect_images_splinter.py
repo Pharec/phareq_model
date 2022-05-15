ec792741c1496bf779877a0fa747c5c850e275d4
Ali <ali>
2022-05-15 06:27:47 -0400

    Modified collection script for roubustness

--- dataset/collect_images_splinter.py
+++ dataset/collect_images_splinter.py
@@ -1,29 +1,40 @@
-import csv
 from splinter import Browser
 from tqdm import tqdm
+from pathlib import Path
+
 
 def collect_images(urls):
     browser = Browser(headless=True)
     # browser = Browser()
 
     width = 1024
-    height = width // 3
+    height = 3 * width // 4
 
     browser.driver.set_window_size(width, height)
     print("Browser size", browser.driver.get_window_size())
     for url in tqdm(urls):
-        browser.visit(url)
+        try:
+            browser.visit(url)
+        except Exception as e:
+            print(f"Failed to visit {url} with exception {e}") 
+            continue
 
-        browser.screenshot(f"/home/falco/phareq_model/dataset/collected_images/img-{url.split('//')[-1]}.png")
+        image_path = (
+            Path("./") / f"collected_images/img_{url.split('//')[-1]}_"
+        ).absolute()
+        browser.screenshot(
+            str(image_path)
+        )
     browser.quit()
 
 if __name__ == "__main__":
+    import csv
     with open('top500Domains.csv') as fp:
         reader = csv.reader(fp)
 
         # Skip header
         _ = next(reader)
-        urls = [f"http://{line[1]}" for line in reader] 
+        urls = [f"http://{line[1]}" for line in reader]
 
     # urls = ['http://google.com', 'http://pharec-dl.tech']
-    collect_images(urls)
+    collect_images(urls[:10])
