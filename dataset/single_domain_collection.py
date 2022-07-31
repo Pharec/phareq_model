bb421300dee7ec4c539b8a25c1c0c4819155516d
Ali <ali>
2022-07-31 07:54:51 -0400

    Added saving crawled urls to single domain experiment

--- dataset/single_domain_collection.py
+++ dataset/single_domain_collection.py
@@ -1,17 +1,19 @@
 """
     Collect a dataset given a single domain (e.g. example.com)
 """
-
-from crawl_urls import crawl_page
+from crawl_urls import crawl_page, save_domain_urls
 from collect_images_splinter import collect_images
 
 
-def collect_one(domain="example.com", limit=100):
+def collect_one(domain_url="example.com", depth=2, limit=200):
     """
         1. Crawl domain site for links
         2. Collect images
     """
-    urls = crawl_page(domain, limit=limit)
+
+    urls = crawl_page(domain_url, depth=depth, limit=limit)
+    save_domain_urls(domain_url, urls)
+
     collect_images(urls)
     return urls
 
@@ -19,4 +21,4 @@ def collect_one(domain="example.com", limit=100):
 if __name__ == "__main__":
     domain_url = "https://splonline.com.sa"
 
-    collect_one(domain_url, limit=None)
+    collect_one(domain_url, depth=0, limit=None)
