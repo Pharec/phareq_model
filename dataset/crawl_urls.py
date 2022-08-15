f3c0cec0c4720a85cab79fa7748e51e8ec50156e
Ali <ali>
2022-08-15 03:41:11 -0400

    Added root url; other fixes

--- dataset/crawl_urls.py
+++ dataset/crawl_urls.py
@@ -12,6 +12,7 @@ from spidy import crawler
 from urllib.request import urlopen
 import mimetypes
 
+
 def guess_type_of(link, strict=True):
     """
         From Stackoverflow (Modified)
@@ -25,25 +26,30 @@ def guess_type_of(link, strict=True):
             print(f"Failed to urlopen {link} to check page type with: {e}")
     return link_type
 
+
 def crawl_page(page_url, depth=2, limit=None):
     """
         Crawl iteratively for a certain depth
     """
-    seen_urls = set()
+    seen_urls = set([page_url])
     url_depth = list()
-    url_depth.append(set(crawl_once(page_url)))
+
+    url_depth.append(set(crawl_once(page_url, limit=limit)))
     seen_urls.update(url_depth[0])
+
     for i in range(depth):
         url_depth.append(set())
         for url in list(url_depth[i]):
-            crawled_urls = set(crawl_once(url))
-            d_urls = crawled_urls - seen_urls
-            url_depth[i+1].update(d_urls)
-            seen_urls.update(d_urls)
             if limit and len(seen_urls) >= limit:
                 return list(seen_urls)[:limit]
 
+            crawled_urls = set(crawl_once(url, limit=limit))
+            d_urls = crawled_urls - seen_urls
+            url_depth[i+1].update(d_urls)
+            seen_urls.update(d_urls)
+
     return list(seen_urls)
+
 
 def crawl_once(page_url, limit=None):
     scheme = urlparse(page_url).scheme
@@ -93,10 +99,7 @@ def check_link(url, page_domain):
         return False
 
     link_domain = tldextract.extract(url).registered_domain
-    if  link_domain != page_domain:
-        return False
-
-    if guess_type_of(url) != 'text/html':
+    if link_domain != page_domain:
         return False
 
     other_protocols = [
@@ -104,25 +107,37 @@ def check_link(url, page_domain):
         "callto:",
         "mailto:",
         "sms:",
+        "data:",
+        "android-app:",
+        "ios-app:",
         "telprompt",
         "javascript"
     ]
     other_protocols += [p.upper() for p in other_protocols]
 
+    path = urlparse(url).path.strip('/')
     for p in other_protocols:
-        if url.split('/')[-1].startswith(p):
+        if path.startswith(p):
             return False
+
+    if guess_type_of(url) != 'text/html':
+        return False
 
     return True
 
 
-def crawl_domains(domain_list, save_dir="crawled_urls", save=True):
-    url_dict = {domain_name: [url for url in crawl_page(domain_name)]
-                for domain_name in domain_list}
+def crawl_domains(domain_list,
+                  save_dir="crawled_urls",
+                  save=True, depth=0, limit=None):
 
-    if save:
-        for domain_name, url_list in url_dict.items():
-            save_domain_urls(domain_name, url_list, save_dir)
+    url_dict = dict()
+    for domain_name in domain_list:
+        urls = crawl_page(domain_name, depth=depth, limit=limit)
+
+        if save:
+            save_domain_urls(domain_name, urls, save_dir)
+
+        url_dict[domain_name] = urls
 
     return url_dict
 
@@ -135,6 +150,11 @@ def save_domain_urls(domain_url, url_list, save_dir="crawled_urls"):
         fp.write('\n'.join(url_list))
 
 
+def read_domains(domains_list_filename='domain_refs/draft1_domains.list'):
+    with open(domains_list_filename, 'r') as fp:
+        return [domain.strip() for domain in fp]
+
+
 if __name__ == "__main__":
     import csv
     with open('top500Domains.csv') as fp:
@@ -145,7 +165,37 @@ if __name__ == "__main__":
         domain_urls = [f"http://{line[1]}" for line in reader]
 
     # domain_url = 'https://splonline.com.sa'
-    domain_url = 'https://kacst.edu.sa'
+    # domain_url = 'https://mcit.gov.sa'
+    # domain_url = 'https://apple.com'
+    # domain_url = 'https://dropbox.com'
+    # domain_url = 'https://amazon.com'
+    # domain_url = 'https://amazon.sa'
+    # domain_url = 'https://noon.com'
+    # domain_url = 'https://xcite.com'
+    # domain_url = 'https://google.com'
+    # domain_url = 'https://aramex.com'
+    # domain_url = 'https://airbnb.com'
+    # domain_url = 'https://paypal.com'
+    # domain_url = 'https://dhl.com'
+    # domain_url = 'https://whatsapp.com'
+    # domain_url = 'https://youtube.com'
+    # domain_url = 'https://tiktok.com'
+    # domain_url = 'https://facebook.com'
+    # domain_url = 'https://twitter.com'
+    # domain_url = 'https://coinbase.com'
+    # domain_url = 'https://rain.com'
+    # domain_url = 'https://binance.com'
+    # domain_url = 'https://bitoasis.net'
+    # domain_url = 'https://coursera.com'
+    # domain_url = 'https://edx.com'
+    # domain_url = 'https://udemy.com'
+    # domain_url = 'https://alahli.com'
+    # domain_url = 'https://alrajhibank.com'
+    # domain_url = 'https://bankaljazira.com'
+    # domain_url = 'https://anb.com.sa'
+    # domain_url = 'https://alinma.com'
 
-    urls = crawl_page(domain_url, depth=0)
+    # urls = crawl_page(domain_url, depth=0)
     # url_dict = crawl_domains(domain_urls, "crawled_urls")
+    domains = read_domains()
+    url_dict = crawl_domains(domains, save=True)
